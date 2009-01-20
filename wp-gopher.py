5527f555afdc4053d409da0bbc4e1840089453b4
Adam Harvey <aharvey@php.net>
2009-01-20 01:05:46 +0000

    Updated copyright messages.

--- wp-gopher.py
+++ wp-gopher.py
@@ -4,7 +4,7 @@
 
 # The MIT License
 # 
-# Copyright (c) 2007 Adam Harvey
+# Copyright (c) 2007-09 Adam Harvey
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
